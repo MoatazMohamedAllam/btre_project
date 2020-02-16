288ca3ecbee171d08c0982149f0e03b51e8eaa33
MoatazMohamedAllam <mezo.allam2015@hotmail.com>
2020-02-16 21:00:17 +0200

    Initial Commit

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/moataz14/Desktop/myprojects/django-projects/Hotel-Project/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
